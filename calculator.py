acf454f910b5366ae4331a71940582c1068ad21c
Nom3o <b3lar00s@gmail.com>
2025-07-17 04:52:17 -0400

    my first simple  calculator for PLP

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#simple calculator
+
+num1 = float(input('Enter First Number: '))
+num2 = float(input('Enter Second Number: '))
+
+
+print('The sum is: ' ,num1+num2)
+print('The multiplication is: ' ,num1*num2)
+print('The sub is: ' ,num1-num2)
+print('The division is: ' ,num1/num2)
+
