e6eea365b4ad2cf18e6f0ee762c9a5bd68e094e1
Nom3o <b3lar00s@gmail.com>
2025-07-29 03:46:44 -0400

    plp coding challenge 1

---
+++ python_Weekly_Code_Challenge!/greeting_app.py
@@ -0,0 +1,15 @@
+"""
+
+1. Personalized Greeting App 👋
+Create a program that asks for the user’s name and favorite color, 
+then prints a personalized greeting like: “Hello, [Name]! 
+Your favorite color, [Color], is awesome!”
+
+"""
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print("Personalized Greeting App 👋")
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+name = input('Hello! What is your name? ')
+color = input('What is your favorite color? ')
+print('Hello, ' , name,'! ','Your favorite color, ', color , ' is awesome!')
