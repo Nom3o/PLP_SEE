c72e6b01ac793827a97cddefb63267e37c3fe3fc
Nom3o <b3lar00s@gmail.com>
2025-07-30 02:26:55 -0400

    tuples 2nd weekly code challenge 2

---
+++ python_Weekly_Code_Challenge!/week_2/tuple.py
@@ -0,0 +1,8 @@
+"""
+Create a tuple containing the names of five of your favorite books.
+Then, use a for loop to print each book name on a separate line.
+"""
+
+books = ('Holy Bible','trading candle', 'Tell me your dreams', 'pinocchio', 'chess for dummies')
+for x in books:
+    print(x)
