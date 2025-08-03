891db274ba8aec6f1d2ecefef02d50e9246fba20
Nom3o <b3lar00s@gmail.com>
2025-08-02 23:09:53 -0400

    testing python 101

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import random
 name = 'Abel Meuriya'
 age = 25
 fav_color = 'Black'
@@ -8,3 +7,4 @@ print(fav_color, 'is the best color buddy!')
 
 country = ['Ethiopia','ghana','south africa','kenya']
 print(country[::-1])
+print(country[1:3])
