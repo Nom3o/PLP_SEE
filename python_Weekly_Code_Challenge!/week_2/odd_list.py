2bd7d28020e9dea0cae2e51052d149c1d6df23d0
Nom3o <b3lar00s@gmail.com>
2025-08-04 02:15:08 -0400

    week 2 code challenge

---
+++ python_Weekly_Code_Challenge!/week_2/odd_list.py
@@ -0,0 +1,10 @@
+"""
+Create a program that stores a list of words. Then, 
+use list comprehension to create a new list that
+ contains only the words that have an odd number of characters.
+"""
+
+words = ['Thor','Loki','Odin','Hela','Heimdall','seven','eight','one','two']
+
+odd_number = [word for word in words if len(word) % 2 !=0]
+print(odd_number)
